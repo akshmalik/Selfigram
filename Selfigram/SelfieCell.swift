6de8c4b51dea422f6c3f518587774b3610ed1e8d
akshmalik <30788834+akshmalik@users.noreply.github.com>
2017-08-23 19:45:06 -0700

    W3D2

---
+++ Selfigram/SelfieCell.swift
@@ -0,0 +1,29 @@
+//
+//  SelfieCell.swift
+//  Selfigram
+//
+//  Created by Aksh Malik on 2017-08-23.
+//  Copyright © 2017 Aksh Malik. All rights reserved.
+//
+
+import UIKit
+
+class SelfieCell: UITableViewCell {
+
+    @IBOutlet weak var selfieImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
